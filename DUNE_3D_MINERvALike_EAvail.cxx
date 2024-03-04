d593f35d8812a74b54c5fb4a356cc4da9ad2a1e1
Abi Peake <abipeake@gmail.com>
2024-03-04 14:41:25 +0000

    new version of DUNE_3D_MINERvALike_EAvail.cxx	with finer binning in pt and pz

--- DUNE_3D_MINERvALike_EAvail.cxx
+++ DUNE_3D_MINERvALike_EAvail.cxx
@@ -55,10 +55,51 @@ public:
     std::vector<double> EAvail_bins = {0.04, 0.08}; // GeV
     */
    // std::vector<double> ptbins = {0.4, 0.475}; // GeV
-    std::vector<double> ptbins = {0,  0.05,   0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5,0.55,0.6,0.65 , 0.7,0.75,0.8 ,0.9, 1.0, 1.5,2.0,  2.5}; // GeV
+
+   /*
+    std::vector<double> ptbins = {0.00, 0.02,0.04, 0.06,0.08, 
+                                  0.1, 0.12,0.14 ,0.16,0.18, 
+                                  0.2, 0.22,0.24,0.26, 0.28, 
+                                  0.3,0.32,0.34 ,0.36,0.38, 
+                                  0.4,0.42, 0.44, 0.46,0.48, 
+                                  0.5,0.52, 0.54, 0.56,0.58, 
+                                  0.6, 0.62, 0.64, 0.66,0.68, 
+                                  0.7,0.72, 0.74, 0.76,0.78, 
+                                  0.8,0.82, 0.84, 0.86,0.88, 
+                                  0.9,0.92, 0.94, 0.96,0.98, 
+                                  1.0, 1.1,1.2,1.3,1.4,1.5, 1.6,1.7,1.8,1.9,
+                                  2.0, 2.1,2.2,2.3,2.4,2.5, 2.6,2.7,2.8,2.9,
+                                }; // GeV */
     //std::vector<double> pzbins = {1.5, 3.5};  // GeV
-    std::vector<double> pzbins = {0.5,1.0,1.5,2,2.5,3, 3.5, 4, 4.5,5.0,6.0, 7.0, 8.0,9.0,10.0, 12.5, 15.0,17.5, 20.0};  // GeV
-    std::vector<double> sumTpbins = {0,    0.02, 0.04, 0.08, 0.12, 0.16, 0.24, 0.32, 0.4,  0.6}; // GeV
+    //std::vector<double> pzbins = {0.5,1.0,1.5,2,2.5,3, 3.5, 4, 4.5,5.0,6.0, 7.0, 8.0,9.0,10.0, 12.5, 15.0,17.5, 20.0};  // GeV
+
+   std::vector<double> pzbins={0.5,  0.55 ,0.6 , 0.65 ,0.7 , 0.75, 0.8 , 0.85, 0.9 , 0.95, 1. ,  1.05 ,1.1,  1.15,
+                                1.2,  1.25 ,1.3 , 1.35 ,1.4 , 1.45, 1.5 , 1.55, 1.6 , 1.65, 1.7,  1.75 ,1.8,  1.85,
+                                1.9 , 1.95, 2.0,   2.05, 2.1 , 2.15, 2.2 , 2.25, 2.3 , 2.35, 2.4,  2.45, 2.5,  2.55,
+                                2.6  ,2.65, 2.7 , 2.75, 2.8 , 2.85, 2.9,  2.95, 3. ,  3.05, 3.1,  3.15, 3.2,  3.25,
+                                3.3 , 3.35, 3.4 , 3.45, 3.5 , 3.55, 3.6 , 3.65, 3.7 , 3.75, 3.8,  3.85, 3.9,  3.95,
+                                4.   ,4.05, 4.1 , 4.15, 4.2 , 4.25, 4.3,  4.35, 4.4 ,4.45, 4.5,  4.55, 4.6,  4.65,
+                                4.7 , 4.75, 4.8 , 4.85, 4.9 , 4.95, 5.0,   5.05, 5.1 , 5.15, 5.2,  5.25, 5.3,  5.35,
+                                5.4 , 5.45, 5.5 , 5.55, 5.6 , 5.65, 5.7,  5.75, 5.8 , 5.85, 5.9,  5.95, 6.0,   6.05,
+                                6.1 , 6.15, 6.2 , 6.25, 6.3 , 6.35, 6.4 , 6.45, 6.5,  6.55, 6.6,  6.65, 6.7, 6.75,
+                                6.8 , 6.85, 6.9 , 6.95, 7.  , 7.05, 7.1  ,7.15, 7.2,  7.25, 7.3,  7.35, 7.4,  7.45,
+                                7.5 , 7.55, 7.6 , 7.65, 7.7 , 7.75, 7.8 , 7.85, 7.9 , 7.95, 8.0,  8.05, 8.1,  8.15,
+                                8.2 , 8.25, 8.3 , 8.35, 8.4 , 8.45, 8.5 , 8.55, 8.6 , 8.65, 8.7,  8.75, 8.8,  8.85,
+                                8.9 , 8.95, 9.0,   9.05, 9.1,  9.15, 9.2,  9.25, 9.3 , 9.35, 9.4,  9.45, 9.5,  9.55,
+                                9.6 , 9.65, 9.7 , 9.75, 9.8 , 9.85 ,9.9  ,9.95,10, 10.2,
+                                10.4,10.6,10.8,12,12.2,12.4,12.6,12.8,13,14,15,16,17,18,19,20}; 
+
+   // std::vector<double> ptbins = {0,  0.05,   0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5,0.55,0.6,0.65 , 0.7,0.75,0.8 ,0.9, 1.0, 1.5,2.0,  2.5}; // GeV
+    std::vector<double> ptbins = {0,  0.05,   0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5,0.55,0.6,0.65, 0.7,0.75,0.8 ,0.85,0.9, 0.95, 1.0, 1.1,1.2 ,1.3,1.4, 1.5,2.0,  2.5}; 
+    //std::vector<double> pzbins = {0.5,1.0,1.5,2,2.5,3, 3.5, 4, 4.5,5.0,6.0, 7.0, 8.0,9.0,10.0, 12.5, 15.0,17.5, 20.0};  // GeV
+
+   /* std::vector<double> pzbins = {0.5,0.75, 1.0, 1.25, 1.5, 1.75, 2,2.25,2.5,2.75, 3, 3.25, 3.5, 3.75,  4, 4.25, 4.5,4.75, 5.0, 5.25, 
+                                  5.5, 5.75, 6.0, 6.25, 6.5, 6.75, 7.0, 7.25, 7.5, 7.75, 8.0, 8.25, 8.5, 8.75, 9.0, 9.25, 9.5, 9.75,
+                                  10.0, 10.25, 10.5, 10.75, 11, 11.25, 11.5, 11.75, 12.0, 12.25, 12.5, 12.75, 13, 13.25, 13.5, 13.75, 14.0, 
+                                  15.0, 16.0, 17, 18.0, 19.0, 20.0};  // GeV*/
+
+
+    std::vector<double> sumTpbins = {0,  0.02, 0.04, 0.08, 0.12, 0.16, 0.24, 0.32, 0.4,  0.6}; // GeV
     std::vector<double> EAvail_bins = {0, 0.01,   0.02, 0.04,0.06,  0.08,0.1, 0.12,0.14, 0.16, 0.2, 0.24, 0.28, 0.32, 0.4, 0.5, 0.6,0.8}; // GeV
 
     // This histogram is just used to help with the binning, we could manually
@@ -121,17 +162,6 @@ public:
         "DUNE_3D_MINERvALike_EAvail_Other", "", ptbins.size() - 1, ptbins.data(),
         pzbins.size() - 1, pzbins.data(), EAvail_bins.size() - 1,
         EAvail_bins.data());
-
-
-    /*
-    // The 1D projections of 3D histograms
-    f1DHist_EAvail_ptpz = std::make_unique<TH1D>("f1DHist_EAvail_ptpz") ;
-    f1DHist_EAvail_CCQE_ptpz = std::make_unique<TH1D>("f1DHist_EAvail_CCQE_ptpz");
-    f1DHist_EAvail_CC2p2h_ptpz = std::make_unique<TH1D>("f1DHist_EAvail_CC2p2h_ptpz");
-    f1DHist_EAvail_CC1p1pi_ptpz = std::make_unique<TH1D>("f1DHist_EAvail_CC1p1pi_ptpz");
-    f1DHist_EAvail_CCDIS_ptpz = std::make_unique<TH1D>("f1DHist_EAvail_CCDIS_ptpz");
-    f1DHist_EAvail_Other_ptpz = std::make_unique<TH1D>("f1DHist_EAvail_Other_ptpz");
-  */
 
     // Now the invariant mass histograms for available energy
     f3DHist_EAvail_lowW =
@@ -198,7 +228,6 @@ public:
     // Set Q2 QE
     double q2qe = 2 * enu_rec * (emu - pmu * cos(thmu)) - mmu * mmu;
 
-
     // Calc Q3 from Q2QE and EnuTree
     q3 = sqrt(q2qe + q0 * q0);
 
@@ -219,10 +248,6 @@ public:
     d3dml_bx->W = W_nuc_rest;
     }
 
-    
-
-
-
     static const double toGeV = 1E-3;
 
     d3dml_bx->p_para = Pmu.Vect().Dot(nudir) * toGeV;
@@ -234,7 +259,6 @@ public:
       d3dml_bx->sum_TProt += prot->KE() * toGeV;
     }
     
-
     // find the bin number along each axis
     int binx = f3DHist->GetXaxis()->FindFixBin(d3dml_bx->p_perp);
     int biny = f3DHist->GetYaxis()->FindFixBin(d3dml_bx->p_para);
